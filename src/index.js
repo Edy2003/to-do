7f16a63335015150a67e879186a9432dc02bde8d
edikk <edikkopcha6@gmail.com>
2023-02-28 00:04:04 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function List(){
+   // const [value, setValue] = useState('');
+    const [listItems, setListItems] = useState([]);
+    const ref = useRef();
+    function createListItem(){
+        setListItems([...listItems, ref.current.value])
+
+    }
+
+    function deleteListItem(i){
+        const oldList = [...listItems];
+        oldList.splice(i,1)
+        setListItems(oldList)
+    }
+
+    function ListItem({onDelete, index, element}){
+        return(
+            <div>
+                <p>{element}</p>
+                <button onClick={()=> onDelete(index)}>delete</button>
+                <button onClick={()=> onDelete(index)}>edit</button>
+            </div>
+        )
+    }
+
+    console.log('before', listItems)
+    return(
+        <div>
+            <input
+                ref={ref}
+                type="text"
+                className="input"
+                //onChange={e => setValue(e.target.value)}
+            />
+            <button onClick={createListItem}>Create</button>
+            <div>
+                {listItems.map((el,i)=>
+                    <ListItem onDelete={deleteListItem} index={i} element={el}/>
+                )}
+            </div>
+        </div>
+    );
+
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <List />,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
